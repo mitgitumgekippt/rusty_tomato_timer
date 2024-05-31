97b54f48bff08d8c56b2006afa22f172cb531c0b
Moritz M. <git0001@t-online.de>
2024-05-31 14:45:04 +0200

    Implementation: Basic Timer, which runs once

--- rust_pomodoro.rs
+++ rust_pomodoro.rs
@@ -1,3 +1,22 @@
+use std::{thread, time};
+
 fn main() {
-    println!("Hello World");
+    timer();
+}
+
+fn timer() {
+    print!("Los geht's! ");
+    print!("Die derzeitige Pomodoro-Zeit ist auf 25min und die Pausen-Zeit ist auf 5min gesetzt.");
+
+    let five_min = time::Duration::from_secs(300);
+    
+    let mut n = 0;
+    while n < 5 {
+        println!("Bereits {} min des Pomodorotimers vergangen!", 5*n);
+        thread::sleep(five_min);
+        n += 1;
+    }
+
+    println!("Super, du hast dir eine Pause verdient!");
+    thread::sleep(five_min);
 }
