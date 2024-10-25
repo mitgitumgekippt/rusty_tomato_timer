70e5c4862c5779dd9db866c459885d749868ae42
Moritz M <git001@t-online.de>
2024-10-25 12:21:23 +0200

    Created Cargo.toml, moved all files to src/

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+mod timer;
+use std::io::Write;
+use std::sync::mpsc::{Receiver, Sender};
+
+// Prompt to Shell
+fn prompt(name: &str) -> String {
+    let mut line = String::new();
+    print!("{}", name);
+    std::io::stdout().flush().unwrap();
+    std::io::stdin()
+        .read_line(&mut line)
+        .expect("Error: Could not read a line");
+
+    return line.trim().to_string();
+}
+
+fn main() {
+    let mut counter = 0;
+
+    loop {
+        let input = prompt("< Tomato-Timer: (p)omodoro | (b)reak | (e)xit >");
+        match input.as_str() {
+            "p" | "pomodoro" => {
+                timer::productive_time();
+                counter += 1;
+            }
+
+            "b" | "break" => {
+                if (counter % 4) == 0 {
+                    timer::break_time(true);
+                } else {
+                    timer::break_time(false);
+                }
+            }
+
+            "e" | "exit" => {
+                break;
+            }
+
+            _ => println!("Invalid Command"),
+        }
+    }
+}
